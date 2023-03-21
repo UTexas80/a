8a9793063a36800286f14c9a9837919e6b276c63
UTexas80 <glen.falk@utexas.edu>
2023-03-21 18:07:52 -0400

    20230321volatility_a
    a. IHSMarkit Archive

--- munge/99-forecast.R
+++ munge/99-forecast.R
@@ -17,9 +17,14 @@ disp           <- Delt(bb.20[,"dn"],bb.20[,"up"])                               
 dispDiff       <- Delt(disp)                                                    # Create Daily Dispersion Difference Pct Column
 xts_bb20_disp  <- cbind(dx_msa_5190, bb.20,disp, dispDiff)[,c(5:10)]
 dt_bb20_disp   <- data.table(xts_bb20_disp, keep.rownames = TRUE)
-# --------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 colnames(xts_bb20_disp)[5:6] <- c("pct", "delta")
 names(dt_bb20_disp)[c(1,6:7)] <- c("date", "pct", "delta")
+# ------------------------------------------------------------------------------
+# data.table IDateTime integer date class       https://tinyurl.com/mr35p42e
+d<-daily.Yesterday[, mean(Price), by = list(hour = hour(as.ITime(lubridate::as_datetime(daily.Yesterday$DataSource_Date))))]]
+d<-data.table::setorder(d,hour)
+# ------------------------------------------------------------------------------
 ################################################################################
 ## Step 00.99: VERSION HISTORY                                               ###
 ################################################################################
